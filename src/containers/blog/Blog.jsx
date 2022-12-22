96a69cf1f0da2f5bbf71bcff112a3b0ca0a95d8c
Divyesh Jethwa <divyeshjethwa00@gmail.com>
2022-12-22 17:07:36 +0530

    Bloga and footer

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Article } from "../../components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
+import "./Blog.css";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article
+            title="This is an example title"
+            date="Sep 26, 2021"
+            imgUrl={blog01}
+          />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article
+            title="This is an example title"
+            date="Sep 26, 2021"
+            imgUrl={blog02}
+          />
+          <Article
+            title="This is an example title"
+            date="Sep 26, 2021"
+            imgUrl={blog03}
+          />
+          <Article
+            title="This is an example title"
+            date="Sep 26, 2021"
+            imgUrl={blog04}
+          />
+          <Article
+            title="This is an example title"
+            date="Sep 26, 2021"
+            imgUrl={blog05}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
